abda33458ce165e331c11416001bcfcae43bff7d
Yelyzaveta Melnyk <lizamelnyk@gmail.com>
2020-11-21 15:25:49 +0200

    UI asset+ testScene + prefabs of camera and player

---
+++ Assets/Project/Scripts/UI/CountdownControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownControl : MonoBehaviour
+{
+    public int countdownTime;
+    public Text countdownDisplay;
+    
+    void Start()
+    {
+        
+    }
+
+  
+    IEnumerator CountdownToEnd()
+    {
+        while (countdownTime > 0)
+        {
+            countdownDisplay.text = countdownTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            countdownTime--;
+        }
+
+
+
+        countdownDisplay.text = "Game Over";
+    }
+
+    
+
+
+}
