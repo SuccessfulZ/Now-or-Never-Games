b403f12594c4405fef6d3cc911bf824f73115fa0
Yelyzaveta Melnyk <lizamelnyk@gmail.com>
2020-11-21 12:56:16 +0200

    PlayerControl (Prefab van works well) + CameraController (need to be fixed, work on it)

---
+++ Assets/Project/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Private Variables
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Moves the car forward based on vertical input
+        transform.Translate(Vector3.back * Time.deltaTime * speed * verticalInput);
+        //Rotates the car based on horizontal input 
+        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
+    }
+}
